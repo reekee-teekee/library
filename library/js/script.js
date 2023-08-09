cfccf287e7b9737f9dda346fb4e66afa493cea40
reekee-teekee <zhenya-d@ya.ru>
2023-08-09 22:33:02 +0700

    feat: add slider carrets and replace welcome image

--- library/js/script.js
+++ library/js/script.js
@@ -1,15 +1,9 @@
-let menuBtn = document.querySelector('.header__burger');
-let menu = document.querySelector('.nav');
+const menuBtn = document.querySelector('.header__burger');
+const menu = document.querySelector('.nav');
+const link = document.querySelector('.menu__item-link')
 
 menuBtn.addEventListener('click', function () {
   menuBtn.classList.toggle('open');
   menu.classList.toggle('open');
+  link.classList.toggle('open');
 });
-
-/* document.addEventListener('mouseup', function (e) {
-
-  if (!menu.contains(e.target)) {
-    menu.style.display = 'none';
-  }
-});
-*/
