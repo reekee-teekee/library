af5b9550644e4de2a055498cc6731fcdc76046c2
reekee-teekee <zhenya-d@ya.ru>
2023-08-27 18:13:06 +0700

    init: start library-part3 task

--- library/js/script.js
+++ library/js/script.js
@@ -1,10 +1,13 @@
 const burger = document.querySelector('.header__burger');
 const nav = document.querySelector('.nav');
+const dropMenuGuest = document.querySelector('.drop-menu__no-auth');
+const dropMenuUser = document.querySelector('.drop-menu__with-auth');
+const profile = document.querySelector('.profile-link');
 
 burger.addEventListener('click', function () {
   nav.classList.toggle('open');
   burger.classList.toggle('open');
-})
+});
 
 nav.addEventListener('click', function (e) {
   if (e.target.matches('a')) {
@@ -19,5 +22,15 @@ window.addEventListener('click', e => {
     nav.classList.remove('open');
     burger.classList.remove('open');
   }
+});
+
+profile.addEventListener('click', function () {
+  dropMenuUser.classList.toggle('open-menu');
 })
 
+window.addEventListener('click', e => {
+
+  if (!target.closest('.drop-menu')) {
+    dropMenuUser.classList.remove('open-menu');
+  }
+});
